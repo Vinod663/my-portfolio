e03965108ab322d2f56fa8b00d2373cca3900823
Vinod663 <vinodfernando048@gmail.com>
2025-05-18 00:57:25 +0530

    Add AOS Animations

--- js/function.js
+++ js/function.js
@@ -7,17 +7,4 @@ window.onload = function () {
     });
 };
 
-//Contact Me
-$('#sendEmailBtn').on('click', function() {
-    Email.send({
-        Host : "s1.maildns.net",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-        message => alert(message)
-    );
-});
+
