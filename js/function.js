a8a6a30f2256ed00b0e6841381c124879494b14d
Vinod663 <vinodfernando048@gmail.com>
2025-05-18 17:16:02 +0530

    Contact me Submit Function Update

--- js/function.js
+++ js/function.js
@@ -7,4 +7,55 @@ window.onload = function () {
     });
 };
 
+$('#sendEmailBtn').on('click', function (e) {
+    /*e.preventDefault();*/
+    let hasError = false;
+    const phone = '94778193985'; // Sri Lankan number (no leading 0)
 
+    // Get form values
+    const name = $('#name').val().trim();
+    const email = $('#email').val().trim();
+    const subject = $('#subject').val().trim();
+    const message = $('#message').val().trim();
+
+    $('#nameError').text('');
+    $('#name').css('border-color', '');
+    $('#emailError').text('');
+    $('#email').css('border-color', '');
+
+    if (name===''||email===''||subject===''||message===''){
+        alert('Please fill all the fields!');
+        return;
+    }
+
+    if (!/^[A-Za-z\s]{3,30}$/.test(name)) {
+        $('#nameError').text('Name must be 3–30 letters only');
+        $('#name').css('border-color', 'red');
+        hasError = true;
+    }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        $('#emailError').text('Enter a valid email');
+        $('#email').css('border-color', 'red');
+        hasError = true;
+    }
+
+    if (hasError) return;
+
+
+   const fullMessage = `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+
+
+    const encodedMessage = encodeURIComponent(fullMessage);
+
+
+    const url =`https://wa.me/${phone}?text=${encodedMessage}`;
+
+
+    window.open(url, '_blank');
+
+    $('#name').val('');
+    $('#email').val('');
+    $('#subject').val('');
+    $('#message').val('');
+});
